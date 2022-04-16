def11f42e8e3f30dfa769151589e2a5b23c94d7c
99ehddud <99ehddud@gmail.com>
2022-04-16 15:41:57 +0900

    Ver.x.1.0 (Make Division & Button)

--- calculator/lib/main.dart
+++ calculator/lib/main.dart
@@ -1,9 +1,7 @@
-import 'package:calculator/main.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
+  runApp(const MaterialApp(
     debugShowCheckedModeBanner: false,
     home: Calculator(),
   ));
@@ -21,25 +19,64 @@ class _CalculatorState extends State<Calculator> {
   Widget build(BuildContext context) {
     final double deviceWidth = MediaQuery.of(context).size.width;
     final double deviceHeight = MediaQuery.of(context).size.height;
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: Center(
-          child: Column(
-            children: [
-              Container(
-                height: deviceHeight * 0.35,
-                color: Colors.yellow,
-              ),
-              Container(
-                height: deviceHeight * 0.65,
-                color: Colors.blueAccent,
-              ),
-            ],
-          ),
+    final double buttonHorizontalMargin = deviceWidth * 0.2 / 5;
+    final double buttonWidth = deviceWidth * 0.8 / 4;
+    final double displayHeight = deviceHeight * 0.3;
+    final double buttonVerticalMargin = deviceHeight * 0.2 / 6;
+    final double buttonHeight = deviceHeight * 0.4 / 5;
+
+    return Scaffold(
+      backgroundColor: const Color.fromRGBO(50, 50, 50, 1),
+      body: Center(
+        child: Column(
+          children: [
+            Container(
+              height: displayHeight,
+              color: Colors.yellow,
+            ),
+            SizedBox(height: buttonVerticalMargin),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(width: buttonHorizontalMargin),
+                ElevatedButton(
+                  onPressed: () {},
+                  style: const ButtonStyle(
+                    tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                  ),
+                  child: const Text("7"),
+                ),
+                SizedBox(width: buttonHorizontalMargin),
+                ElevatedButton(
+                  onPressed: () {},
+                  style: const ButtonStyle(
+                    tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                  ),
+                  child: const Text("8"),
+                ),
+                SizedBox(width: buttonHorizontalMargin),
+                ElevatedButton(
+                  onPressed: () {},
+                  style: const ButtonStyle(
+                    tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                  ),
+                  child: const Text("9"),
+                ),
+                SizedBox(width: buttonHorizontalMargin),
+                ElevatedButton(
+                  onPressed: () {},
+                  style: const ButtonStyle(
+                    tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                  ),
+                  child: const Text("1"),
+                ),
+                SizedBox(width: buttonHorizontalMargin),
+              ],
+            ),
+            SizedBox(height: deviceHeight * 0.1),
+          ],
         ),
       ),
     );
   }
 }
-
